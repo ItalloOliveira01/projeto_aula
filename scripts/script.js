5e31eed50b2c9729e0e09895559f93b3bfb36759
Itallogoc <itallogoc@gmail.com>
2025-09-19 01:24:21 -0300

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,23 @@ myImage.onclick = () => {
         myImage.setAttribute("src", "image/html.png");
     };
 };
+
+//mensagens de boas vindas personalizadas
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName(){
+  let myName = prompt("Por favor, digite seu nome: ");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `JavaScript é muito legal, ${myName}`;
+}
+if(!localStorage.getItem("name")){
+  setUserName();
+}else{
+  let storredName = localStorage.getItem("name");
+  myHeading.textContent = `JavvaScript é muito legal, ${storredName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+}
